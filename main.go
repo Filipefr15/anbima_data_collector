9a7e42c3a10dfee672faf8005c246529b08e0c17
Filipe R. Ferreira <filipe-ferreira-1@hotmail.com>
2025-10-18 14:54:32 -0400

    feat: add rota infdiarioultimosdias

--- main.go
+++ main.go
@@ -270,6 +270,7 @@ func startServerRegistroFundo() {
 	http.HandleFunc("/dashboard/registrofundo/top10Fundos", corsMiddleware(registroFundoTop10FundosHandler))
 	http.HandleFunc("/dashboard/registrofundo/distsit", corsMiddleware(registroFundoDistSitHandler))
 	http.HandleFunc("/dashboard/registrofundo/timeline", corsMiddleware(registroFundoTimelineAberturaFundosHandler))
+	http.HandleFunc("/infdiarioultimosdias", corsMiddleware(infDiarioUltimosDiasFundosHandler))
 	fmt.Println("Servidor RegistroFundo iniciado em :8080")
 	fmt.Println("Acesse: http://localhost:8080/registrofundo")
 	http.ListenAndServe(":8080", nil)
@@ -681,6 +682,80 @@ func registroFundoTimelineAberturaFundosHandler(w http.ResponseWriter, r *http.R
 		err = rows.Scan(
 			&fundo.Periodo,
 			&fundo.Quantidade,
+		)
+		if err != nil {
+			http.Error(w, "Erro ao fazer scan dos dados: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fundos = append(fundos, fundo)
+	}
+
+	if err = rows.Err(); err != nil {
+		http.Error(w, "Erro durante iteração das linhas: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Configura o header para JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	// Codifica e retorna o JSON
+	if err := json.NewEncoder(w).Encode(fundos); err != nil {
+		http.Error(w, "Erro ao codificar JSON: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func infDiarioUltimosDiasFundosHandler(w http.ResponseWriter, r *http.Request) {
+
+	cnpj_fundo := r.URL.Query().Get("cnpj_fundo")
+	mes := r.URL.Query().Get("mes")
+	ano := r.URL.Query().Get("ano")
+
+	var noParams bool = false
+	if cnpj_fundo != "" || mes != "" || ano != "" {
+		noParams = true
+	}
+
+	mapInfo := map[string]string{
+		"cnpj_fundo_classe": cnpj_fundo,
+		"mes":               mes,
+		"ano":               ano,
+	}
+
+	// Conecta ao banco
+	db, err := conectaDB()
+	if err != nil {
+		http.Error(w, "Erro ao conectar ao banco de dados: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	// Executa a consulta
+	rows, err := executarConsultaWithParams(db, "sql/inf_diario_ultimos_dias.sql", mapInfo, noParams)
+	if err != nil {
+		http.Error(w, "Erro ao executar consulta: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	// Processa os resultados
+	var fundos []models.InformeDiarioLastDays
+	for rows.Next() {
+		var fundo models.InformeDiarioLastDays
+		err = rows.Scan(
+			&fundo.TpFundoClasse,
+			&fundo.CNPJFundoClasse,
+			&fundo.IdSubclasse,
+			&fundo.DtComptc,
+			&fundo.VlTotal,
+			&fundo.VlQuota,
+			&fundo.VlPatrimLiq,
+			&fundo.CaptcDia,
+			&fundo.ResgDia,
+			&fundo.NrCotst,
+			&fundo.Dia,
+			&fundo.Mes,
+			&fundo.Ano,
 		)
 		if err != nil {
 			http.Error(w, "Erro ao fazer scan dos dados: "+err.Error(), http.StatusInternalServerError)
